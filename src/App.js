f99d8ab0a90850d4673fdbb3772f81a24c4be53d
gabriel <gabzkinyua@gmail.com>
2025-01-21 13:49:05 +0300

    Added images and basic styling

--- src/App.js
+++ src/App.js
@@ -3,15 +3,41 @@ import './App.css';
 
 const App = () => {
   const products = [
-    { "id": 1, "name": "T-Shirt", "price": 20 },
+    { 
+      id: 1, 
+      name: "T-Shirt", 
+      price: 20, 
+      image: "https://cdn.pixabay.com/photo/2024/04/29/04/21/tshirt-8726721_1280.jpg"
+    },
+      
+    { 
+      id: 2, 
+      name: "Jeans", 
+      price: 40, 
+      image: "https://images.unsplash.com/photo-1542272604-787c3835535d?q=80&w=1926&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+      
+    { 
+      id: 3, 
+      name: "Sneakers", 
+      price: 60, 
+      image: "https://images.unsplash.com/photo-1465453869711-7e174808ace9?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+      
+    { 
+      id: 4, 
+      name: "Hat", 
+      price: 15, 
+      image: "https://images.unsplash.com/photo-1556306535-0f09a537f0a3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    { 
+      id: 5, 
+      name: "Socks", 
+      price: 20, 
+      image: "https://images.unsplash.com/photo-1535486648131-54a1558cb3fc?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+      
 
-    { "id": 2, "name": "Jeans", "price": 40 },
-  
-    { "id": 3, "name": "Sneakers", "price": 60 },
-  
-    { "id": 4, "name": "Hat", "price": 15 },
-  
-    { "id": 5, "name": "Socks", "price": 5 }
   ];
 
   // State of the cart
@@ -85,14 +111,17 @@ const App = () => {
     </div>
   );
 };
-
+//  ProductList  components
 const ProductList = ({products, addToCart}) => {
   return (
-    <div>
+    <div className='product-list'>
       {products.map(product => (
-        <div key={product.id}>
+        <div key={product.id} className='product'>
           <h3>{product.name}</h3>
           <p>Price: ${product.price}</p>
+          <div className='image-container'>
+          <img src={product.image} alt={product.name} />
+          </div>
           <button onClick={() => addToCart(product)}>Add to cart</button>
         </div>
       ))}
@@ -100,11 +129,12 @@ const ProductList = ({products, addToCart}) => {
   );
 };
 
+// CartSummary component
 const CartSummary = ({cart, clearCart, checkout, removeFromCart, increaseQuantity, decreaseQuantity}) => {
   const totalPrice = cart.reduce((total, item) => total + item.price * item.quantity, 0);
 
   return (
-    <div>
+    <div className='cart'>
       <h2>Cart</h2>
       {cart.length === 0 && <p>Your cart is empty</p>}
       {cart.map(item => (
@@ -120,9 +150,11 @@ const CartSummary = ({cart, clearCart, checkout, removeFromCart, increaseQuantit
           <button onClick={() => removeFromCart(item)}>Remove</button>
         </div>
       ))}
+      <div className='cart-summary'>
       <p>Total: ${totalPrice}</p>
       <button onClick={clearCart}>Clear Cart</button>
       {cart.length > 0 && <button onClick={checkout}>Checkout</button>}
+      </div>
     </div>
   );
 
